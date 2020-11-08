d5f8a9f08d8ac63f03b9f712cb43f0d363639b44
unknown <baggi700@mail.ru>
2020-11-08 22:50:47 +0300

    v1

---
+++ src/main/java/ru/appline/controller/Controller.java
@@ -0,0 +1,48 @@
+package ru.appline.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.appline.logic.Pet;
+import ru.appline.logic.PetModel;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class Controller {
+    private static final PetModel petModel = PetModel.getInstance();
+    private static final AtomicInteger newId = new AtomicInteger(1);
+
+    @PostMapping(value = "/createPet", consumes = "application/json", produces = "application/json")
+    public String createPet(@RequestBody Pet pet){
+        int id = newId.getAndIncrement();
+        petModel.add(pet, id);
+        if (id == 1) {
+            return "{\n \"Answer\": \"Поздравляем, вы создали вашего первого домашнего питомца!\"\n}";
+        } else {
+            return "{\n \"Answer\": \"Поздравляем, вы создали домашнего питомца!\"\n}";
+        }
+    }
+
+    @GetMapping(value = "/getAll", produces = "application/json")
+    public Map<Integer, Pet> getAll() {
+        return petModel.getAll();
+    }
+
+    @GetMapping(value = "/getPet", consumes = "application/json", produces = "application/json")
+    public Pet getPet(@RequestBody Map<String, Integer> id) {
+        return petModel.getFromList(id.get("id"));
+    }
+
+    //Удаляет питомца с указанным id, если такой существует
+    @DeleteMapping(value = "/deletePet", consumes = "application/json")
+    public void deletePet(@RequestBody Map<String, Integer> id) {
+        petModel.deletePet(id.get("id"));
+    }
+
+    //Изменяет питомца с заданным id, если такой существует. Не создаёт нового питомца при отсутствии записи с нужным id.
+    @PutMapping(value = "/updatePet", consumes = "application/json")
+    public void updatePet(@RequestBody Map<String, String> id) {
+        petModel.updatePet(Integer.parseInt(id.get("id")), new Pet(id.get("name"), id.get("type"), Integer.parseInt(id.get("age"))));
+    }
+
+}
---
+++ src/main/java/ru/appline/logic/PetModel.java
@@ -0,0 +1,41 @@
+package ru.appline.logic;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PetModel implements Serializable {
+    private static final PetModel instance = new PetModel();
+
+    private final Map<Integer, Pet> model;
+
+    public PetModel() {
+        model = new HashMap<Integer, Pet>();
+    }
+
+    public static PetModel getInstance() {
+        return instance;
+    }
+
+    public void add(Pet pet, int id) {
+        model.put(id, pet);
+    }
+
+    public Pet getFromList(int id) {
+        return model.get(id);
+    }
+
+    public Map<Integer, Pet> getAll() {
+        return model;
+    }
+
+    public void deletePet(int id) {
+        model.remove(id);
+    }
+
+    public void updatePet(int id, Pet pet) {
+        if (model.containsKey(id)) {
+            model.put(id, pet);
+        }
+    }
+}
